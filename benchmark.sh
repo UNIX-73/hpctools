9104f8f2d55a132e8b5fef81d8c04c0422a1ddea
Unai Iborra <iborra.unai@gmail.com>
2025-10-13 23:22:57 +0200

    Changed the benchmark.sh slurm job to use 12h instead of 48

--- benchmark.sh
+++ benchmark.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=hpc_tools_compiler_vectorization
 #SBATCH --output=job_results.out
 #SBATCH --error=job_results.err
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=32G
 #SBATCH --ntasks=1
