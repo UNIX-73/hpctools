069d01a2c8518b75741d26623ec95cf2d5f94d79
Unai <iborra.unai@gmail.com>
2025-12-07 19:25:23 +0100

    Benchmarking script for task 3 created

---
+++ benchmark_1c_task3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --exclusive
+#SBATCH --job-name=hpc_tools_compiler_vectorization
+#SBATCH --output=job_results_1c.out
+#SBATCH --error=job_results_1c.err
+#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+
+module load cesga/2025 openblas imkl
+
+python scripts/benchmark_1c_task3.py
